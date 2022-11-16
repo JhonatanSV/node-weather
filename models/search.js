cd94476994ffd37071b5a9c95a2ece10e4b37f54
JhonatanSV <jsandoval@vichara.com>
2022-11-15 23:40:12 -0500

    project completed

--- models/search.js
+++ models/search.js
@@ -1,23 +1,100 @@
+const fs = require("fs");
 const axios = require("axios");
 
 class Search {
-    historial = [""];
+  historial = [];
+  dbPath = "./db/database.json";
 
-    constructor() {
+  constructor() {
+    this.readDB();
+  }
 
+  get paramsMapbox() {
+    return {
+      access_token: process.env.MAPBOX_KEY,
+      limit: 5,
+      language: "es",
+    };
+  }
+
+  get paramsWeather() {
+    return {
+      appid: process.env.OPENWEATHER_KEY,
+      lang: "es",
+      units: "metric",
+    };
+  }
+
+  get capitalCaseHistorial() {
+    return this.historial.map((place) => {
+      return `${place[0].toLocaleUpperCase()}${place.slice(1, place.length)}`;
+    });
+  }
+
+  async city(place = "") {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?`,
+        params: this.paramsMapbox,
+      });
+
+      const resp = await instance.get();
+      return resp.data.features.map((place) => ({
+        id: place.id,
+        name: place.place_name,
+        lng: place.center[0],
+        lat: place.center[1],
+      }));
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async weather(lat = "", lon = "") {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: { ...this.paramsWeather, lat, lon },
+      });
+
+      const resp = await instance.get();
+      const { weather, main } = resp.data;
+
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async addHistorial(place = "") {
+    if (this.historial.includes(place.toLocaleLowerCase())) {
+      return;
     }
 
-    async city(place = "") {
-        try {
-            const resp = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?limit=5&language=es&access_token=pk.eyJ1IjoiamhvbnRnb3ciLCJhIjoiY2xhaGkxNDVjMTN3eTNwbzVkdDJseThzeCJ9.k6tw6UzLaWjuuDGLgm9ypA`);
-            console.log(resp)
+    this.historial.unshift(place.toLocaleLowerCase());
+    this.saveDB();
+  }
 
-        } catch (error) {
-            return [];
-        }
+  saveDB() {
+    const payload = {
+      historial: this.historial,
+    };
+
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+
+  readDB() {
+    if (fs.existsSync(this.dbPath)) {
+      this.historial = JSON.parse(
+        fs.readFileSync(this.dbPath, { encoding: "utf-8" })
+      ).historial;
     }
-
-
+  }
 }
 
 module.exports = Search;
